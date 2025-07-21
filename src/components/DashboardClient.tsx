7d58bd1b721ae8969e552edd2d04ad07d049a322
Alvin Ortono <acortono@gmail.com>
2025-07-21 14:38:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/DashboardClient.tsx
+++ src/components/DashboardClient.tsx
@@ -41,7 +41,8 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogClose
+  DialogClose,
+  DialogFooter
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -346,5 +347,7 @@ export function DashboardClient() {
     </div>
   );
 }
+
+    
 
     
