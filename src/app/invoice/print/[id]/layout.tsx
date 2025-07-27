12d11e2623f9030c3642ec7a07cc01c84fbcb94e
Alvin Ortono <acortono@gmail.com>
2025-07-27 07:06:36 +0000

    FIX:Error: A tree hydrated but some attributes of the server rendered HT

--- src/app/invoice/print/[id]/layout.tsx
+++ src/app/invoice/print/[id]/layout.tsx
@@ -12,22 +12,8 @@ export default function PrintLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body bg-white">
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }
