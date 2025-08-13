e186385bf96e9f91e5a96ea90454e09d83ef2fb7
Alvin Ortono <acortono@gmail.com>
2025-08-13 17:37:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <PrintReportPage>

--- src/app/invoice/print/[id]/layout.tsx
+++ src/app/invoice/print/[id]/layout.tsx
@@ -12,8 +12,10 @@ export default function PrintLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      {children}
-    </>
+     <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
--- src/app/reports/print/layout.tsx
+++ src/app/reports/print/layout.tsx
@@ -11,8 +11,10 @@ export default function PrintLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      {children}
-    </>
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
