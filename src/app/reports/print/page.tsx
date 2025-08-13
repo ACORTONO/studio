4d0701f5b308317d2e6145da860971a56fd86129
Alvin Ortono <acortono@gmail.com>
2025-08-13 17:35:16 +0000

    REMOVED BACKGROUND ON THE PRINTABLE REPORT CONTENT

--- src/app/reports/print/page.tsx
+++ src/app/reports/print/page.tsx
@@ -59,7 +59,7 @@ export default function PrintReportPage() {
 
 
     return (
-        <div className="bg-gray-100 min-h-screen">
+        <div className="min-h-screen">
             <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8 space-y-4 no-print">
                 <div className="flex justify-between items-center text-gray-800">
                     <div>
