f3e946cbc262012054f5b8f518c6a18877913b34
Alvin Ortono <acortono@gmail.com>
2025-10-16 05:34:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "job-order-kd84e",
+  "appId": "1:626062212466:web:fed11382080ff3948431eb",
+  "apiKey": "AIzaSyDXr4RuP0ejH62ls2rSqxwtGUiczg2zgK8",
+  "authDomain": "job-order-kd84e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626062212466"
+};
